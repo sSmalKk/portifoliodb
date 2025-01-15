2abdc320ccf9788ae05b21273ca41684e1c89213
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-15 20:52:32 -0300

    Update socketchat.js

--- services/socket/socketchat.js
+++ services/socket/socketchat.js
@@ -1,31 +1,30 @@
-/**
- * socket.js
- * @description :: socket connection with server
- */
+module.exports = function (httpServer) {
+  const io = require("socket.io")(httpServer, { cors: { origin: "*" } });
 
-const socketData = require('../../model/socketData');
-const dbService = require('../../utils/dbService');
-module.exports = function (httpServer){
-  const io = require('socket.io')(httpServer,{ cors:{ origin: '*' } });
-  io.on('connection', (socket) => {
-    io.on('event', async (data) => {
-      if (data.message){
-        const user = await dbService.findOne(socketData,{ message:data.message });
-        if (user){
-          await dbService.updateOne(socketData,{ message:data.message },{ socketId:socket.id });
-        }
-        else {
+  io.on("connection", (socket) => {
+    console.log(`Novo cliente conectado: ${socket.id}`);
+
+    // Evento associado ao socket conectado
+    socket.on("event", async (data) => {
+      if (data.message) {
+        const user = await dbService.findOne(socketData, { message: data.message });
+        if (user) {
+          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
+        } else {
           const input = new socketData({
-            message:data.message,
-            socketId: socket.id
+            message: data.message,
+            socketId: socket.id,
           });
-          await dbService.create(socketData,input);
+          await dbService.create(socketData, input);
         }
-      }
-      else {
+      } else {
         const input = new socketData({ socketId: socket.id });
-        await dbService.create(socketData,input);
+        await dbService.create(socketData, input);
       }
+    });
+
+    socket.on("disconnect", () => {
+      console.log(`Cliente desconectado: ${socket.id}`);
     });
   });
 };
