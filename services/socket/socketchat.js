ac85b55656c6bd3251e22d1503574f2fc47500a7
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-15 21:07:47 -0300

    a

--- services/socket/socketchat.js
+++ services/socket/socketchat.js
@@ -1,30 +1,31 @@
-module.exports = function (httpServer) {
-  const io = require("socket.io")(httpServer, { cors: { origin: "*" } });
+/**
+ * socket.js
+ * @description :: socket connection with server
+ */
 
-  io.on("connection", (socket) => {
-    console.log(`Novo cliente conectado: ${socket.id}`);
-
-    // Evento associado ao socket conectado
-    socket.on("event", async (data) => {
-      if (data.message) {
-        const user = await dbService.findOne(socketData, { message: data.message });
-        if (user) {
-          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
-        } else {
-          const input = new socketData({
-            message: data.message,
-            socketId: socket.id,
-          });
-          await dbService.create(socketData, input);
+const socketData = require('../../model/socketData');
+const dbService = require('../../utils/dbService');
+module.exports = function (httpServer){
+  const io = require('socket.io')(httpServer,{ cors:{ origin: '*' } });
+  io.on('connection', (socket) => {
+    io.on('event', async (data) => {
+      if (data.message){
+        const user = await dbService.findOne(socketData,{ message:data.message });
+        if (user){
+          await dbService.updateOne(socketData,{ message:data.message },{ socketId:socket.id });
         }
-      } else {
-        const input = new socketData({ socketId: socket.id });
-        await dbService.create(socketData, input);
+        else {
+          const input = new socketData({
+            message:data.message,
+            socketId: socket.id
+          });
+          await dbService.create(socketData,input);
+        }
       }
-    });
-
-    socket.on("disconnect", () => {
-      console.log(`Cliente desconectado: ${socket.id}`);
+      else {
+        const input = new socketData({ socketId: socket.id });
+        await dbService.create(socketData,input);
+      }
     });
   });
 };
