81b281ccabd97bf1d3cc86855345e1a6b5067ccd
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-15 21:35:10 -0300

    Update socketchat.js

--- services/socket/socketchat.js
+++ services/socket/socketchat.js
@@ -1,37 +1,39 @@
-/**
- * socket.js
- * @description :: socket connection with server
- */
-
 const socketData = require('../../model/socketData');
 const dbService = require('../../utils/dbService');
+
 module.exports = function (httpServer) {
-  const io = require("socket.io")(httpServer, { cors: { origin: "*" } });
+  const io = require('socket.io')(httpServer, { cors: { origin: '*' } });
 
-  io.on("connection", (socket) => {
-    console.log(`Novo cliente conectado: ${socket.id}`);
+  io.on('connection', (socket) => {
+    console.log('Cliente conectado:', socket.id);
 
-    // Evento associado ao socket conectado
-    socket.on("event", async (data) => {
-      if (data.message) {
-        const user = await dbService.findOne(socketData, { message: data.message });
-        if (user) {
-          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
+    // Receber evento de mensagem
+    socket.on('event', async (data) => {
+      try {
+        if (data && data.message) {
+          const user = await dbService.findOne(socketData, { message: data.message });
+          if (user) {
+            await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
+          } else {
+            const input = new socketData({
+              message: data.message,
+              socketId: socket.id,
+            });
+            await dbService.create(socketData, input);
+          }
+          // Enviar mensagem para todos os sockets conectados
+          io.emit('message', data);
         } else {
-          const input = new socketData({
-            message: data.message,
-            socketId: socket.id,
-          });
-          await dbService.create(socketData, input);
+          throw new Error('Dados de mensagem inválidos.');
         }
-      } else {
-        const input = new socketData({ socketId: socket.id });
-        await dbService.create(socketData, input);
+      } catch (error) {
+        console.error('Erro ao processar mensagem:', error);
+        socket.emit('error', { message: 'Erro ao processar mensagem no servidor.' });
       }
     });
 
-    socket.on("disconnect", () => {
-      console.log(`Cliente desconectado: ${socket.id}`);
+    socket.on('disconnect', () => {
+      console.log('Cliente desconectado:', socket.id);
     });
   });
 };
