0fc492f8dc864db2bcc6218d40334dd810d562a2
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-17 22:54:33 -0300

    Update adminPassportStrategy.js

--- config/adminPassportStrategy.js
+++ config/adminPassportStrategy.js
@@ -8,7 +8,7 @@ const {
   Strategy, ExtractJwt 
 } = require('passport-jwt');
 const { JWT } = require('../constants/authConstant');
-const User = require('../model/user');
+const User = require('../model/User');
 
 const adminPassportStrategy = (passport) => {
   const options = {};
