3e3dd84fc4eaa1533e1648c901d6809e1377419d
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-18 10:54:08 -0300

    up

--- services/socket/index.js
+++ services/socket/index.js
@@ -1,31 +1,53 @@
-/**
- * socket.js
- * @description :: socket connection with server
- */
-
+const ServerClock = require('../../model/ServerClock');
+const PlayerClock = require('../../model/PlayerClock');
 const socketData = require('../../model/socketData');
 const dbService = require('../../utils/dbService');
-module.exports = function (httpServer){
-  const io = require('socket.io')(httpServer,{ cors:{ origin: '*' } });
+
+// Inicialização do relógio global
+const serverClock = new ServerClock('2025-01-18T00:00:00Z');
+
+// Gerenciamento de conexões de jogadores
+const players = {};
+module.exports = function (httpServer) {
+  const io = require('socket.io')(httpServer, { cors: { origin: '*' } });
+
   io.on('connection', (socket) => {
+    // Envia o tick global atual ao conectar
+    const currentTick = serverClock.getCurrentTick();
+    const playerClock = new PlayerClock(currentTick);
     io.on('event', async (data) => {
-      if (data.message){
-        const user = await dbService.findOne(socketData,{ message:data.message });
-        if (user){
-          await dbService.updateOne(socketData,{ message:data.message },{ socketId:socket.id });
+      if (data.message) {
+        const user = await dbService.findOne(socketData, { message: data.message });
+        if (user) {
+          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
         }
         else {
           const input = new socketData({
-            message:data.message,
+            message: data.message,
             socketId: socket.id
           });
-          await dbService.create(socketData,input);
+          await dbService.create(socketData, input);
         }
       }
       else {
         const input = new socketData({ socketId: socket.id });
-        await dbService.create(socketData,input);
+        await dbService.create(socketData, input);
       }
+    });
+    players[socket.id] = playerClock;
+
+    socket.emit('sync', {
+      serverTick: currentTick,
+      worldDate: serverClock.getWorldDate().format(),
+    });
+
+    // Inicia o relógio do jogador
+    playerClock.start();
+
+    // Desconexão do jogador
+    socket.on('disconnect', () => {
+      playerClock.stop();
+      delete players[socket.id];
     });
   });
 };
