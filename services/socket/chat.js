1890e2ebdc611bdb506d078a58b1590688d44779
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-18 10:53:58 -0300

    Update chat.js

--- services/socket/chat.js
+++ services/socket/chat.js
@@ -1,7 +1,31 @@
-module.exports = (io, socket, getTick) => {
-  socket.on('sendMessage', (message) => {
-    const tick = getTick();
-    io.emit('chatMessage', { message, tick });
-    console.log(`Mensagem enviada no tick ${tick}: ${message}`);
+/**
+ * socket.js
+ * @description :: socket connection with server
+ */
+
+const socketData = require('../../model/socketData');
+const dbService = require('../../utils/dbService');
+module.exports = function (httpServer){
+  const io = require('socket.io')(httpServer,{ cors:{ origin: '*' } });
+  io.on('connection', (socket) => {
+    io.on('event', async (data) => {
+      if (data.message){
+        const user = await dbService.findOne(socketData,{ message:data.message });
+        if (user){
+          await dbService.updateOne(socketData,{ message:data.message },{ socketId:socket.id });
+        }
+        else {
+          const input = new socketData({
+            message:data.message,
+            socketId: socket.id
+          });
+          await dbService.create(socketData,input);
+        }
+      }
+      else {
+        const input = new socketData({ socketId: socket.id });
+        await dbService.create(socketData,input);
+      }
+    });
   });
 };
