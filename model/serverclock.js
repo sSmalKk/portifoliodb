8b1521e290693d383c653c378c0273fcfe4043bf
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-18 10:57:08 -0300

    a

--- model/serverclock.js
+++ model/serverclock.js
@@ -1,55 +1,25 @@
-const ServerClock = require('../../model/ServerClock');
-const PlayerClock = require('../../model/PlayerClock');
-const socketData = require('../../model/socketData');
-const dbService = require('../../utils/dbService');
+const moment = require('moment');
 
-// Inicialização do relógio global
-const serverClock = new ServerClock('2025-01-18T00:00:00Z');
+class ServerClock {
+  constructor(startDate, ticksPerSecond = 3, ticksPerMinute = 3) {
+    this.startTime = Date.now(); // Timestamp do início do servidor
+    this.startDate = moment(startDate); // Data inicial do mundo
+    this.ticksPerSecond = ticksPerSecond;
+    this.ticksPerMinute = ticksPerMinute;
+  }
 
-// Gerenciamento de conexões de jogadores
-const players = {};
+  // Retorna o tick global atual com base no tempo
+  getCurrentTick() {
+    const elapsedSeconds = Math.floor((Date.now() - this.startTime) / 1000);
+    return elapsedSeconds * this.ticksPerSecond;
+  }
 
-module.exports = function (httpServer) {
-  const io = require('socket.io')(httpServer, { cors: { origin: '*' } });
+  // Calcula a data do mundo com base no tick global
+  getWorldDate() {
+    const currentTick = this.getCurrentTick();
+    const minutesPassed = Math.floor(currentTick / this.ticksPerMinute);
+    return this.startDate.clone().add(minutesPassed, 'minutes');
+  }
+}
 
-  io.on('connection', (socket) => {
-    // Envia o tick global atual ao conectar
-    const currentTick = serverClock.getCurrentTick();
-    const playerClock = new PlayerClock(currentTick);
-
-    players[socket.id] = playerClock;
-
-    socket.emit('sync', {
-      serverTick: currentTick,
-      worldDate: serverClock.getWorldDate().format(),
-    });
-
-    // Inicia o relógio do jogador
-    playerClock.start();
-
-    // Evento personalizado: Recebe dados e atualiza o banco
-    socket.on('event', async (data) => {
-      if (data.message) {
-        const user = await dbService.findOne(socketData, { message: data.message });
-        if (user) {
-          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
-        } else {
-          const input = new socketData({
-            message: data.message,
-            socketId: socket.id,
-          });
-          await dbService.create(socketData, input);
-        }
-      } else {
-        const input = new socketData({ socketId: socket.id });
-        await dbService.create(socketData, input);
-      }
-    });
-
-    // Desconexão do jogador
-    socket.on('disconnect', () => {
-      playerClock.stop();
-      delete players[socket.id];
-    });
-  });
-};
+module.exports = ServerClock;
--- services/socket/index.js
+++ services/socket/index.js
@@ -1,4 +1,4 @@
-const ServerClock = require('../../model/ServerClock').default;
+const ServerClock = require('../../model/ServerClock');
 const PlayerClock = require('../../model/PlayerClock');
 const socketData = require('../../model/socketData');
 const dbService = require('../../utils/dbService');
@@ -8,6 +8,7 @@ const serverClock = new ServerClock('2025-01-18T00:00:00Z');
 
 // Gerenciamento de conexões de jogadores
 const players = {};
+
 module.exports = function (httpServer) {
   const io = require('socket.io')(httpServer, { cors: { origin: '*' } });
 
@@ -15,25 +16,7 @@ module.exports = function (httpServer) {
     // Envia o tick global atual ao conectar
     const currentTick = serverClock.getCurrentTick();
     const playerClock = new PlayerClock(currentTick);
-    io.on('event', async (data) => {
-      if (data.message) {
-        const user = await dbService.findOne(socketData, { message: data.message });
-        if (user) {
-          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
-        }
-        else {
-          const input = new socketData({
-            message: data.message,
-            socketId: socket.id
-          });
-          await dbService.create(socketData, input);
-        }
-      }
-      else {
-        const input = new socketData({ socketId: socket.id });
-        await dbService.create(socketData, input);
-      }
-    });
+
     players[socket.id] = playerClock;
 
     socket.emit('sync', {
@@ -43,6 +26,25 @@ module.exports = function (httpServer) {
 
     // Inicia o relógio do jogador
     playerClock.start();
+
+    // Evento personalizado: Recebe dados e atualiza o banco
+    socket.on('event', async (data) => {
+      if (data.message) {
+        const user = await dbService.findOne(socketData, { message: data.message });
+        if (user) {
+          await dbService.updateOne(socketData, { message: data.message }, { socketId: socket.id });
+        } else {
+          const input = new socketData({
+            message: data.message,
+            socketId: socket.id,
+          });
+          await dbService.create(socketData, input);
+        }
+      } else {
+        const input = new socketData({ socketId: socket.id });
+        await dbService.create(socketData, input);
+      }
+    });
 
     // Desconexão do jogador
     socket.on('disconnect', () => {
