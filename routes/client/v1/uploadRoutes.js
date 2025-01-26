1f8ad2ac0ca0f454b3d61f188c687d4dc9e2002a
Gustavo Smalk <gustav.o.g@hotmail.com>
2025-01-25 23:07:02 -0300

    Update uploadRoutes.js

--- routes/client/v1/uploadRoutes.js
+++ routes/client/v1/uploadRoutes.js
@@ -8,6 +8,5 @@ const router = express.Router();
 const fileUploadController = require('../../../controller/client/v1/fileUploadController');
 
 router.post('/client/api/v1/upload', fileUploadController.upload);
-router.post('/client/api/v1/upload/list', fileUploadController.listFiles);
 
 module.exports = router;
